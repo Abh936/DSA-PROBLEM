f4812a501a07d141c12d4fdf420ad27fce13c9a4
Abhishek Anand <anand141881@gmail.com>
2025-11-13 19:02:22 +0530

    Time: 7 ms (100.00%) | Memory: 47.4 MB (98.71%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,15 +1,16 @@
 class NumArray {
     int[] arr;
     public NumArray(int[] nums) {
-        arr = nums;
+        arr = Arrays.copyOf(nums,nums.length);
+        for(int i = 1;i<nums.length;i++){
+            arr[i] = arr[i]+arr[i-1];
+        }
     }
     
     public int sumRange(int left, int right) {
-        int sum = 0;
-        for(int i = left;i<=right;i++){
-            sum = sum+arr[i];
+        if(left==0) return arr[right];
+             return arr[right] - arr[left-1];
         }
-        return sum;
     }
-}
+
 
