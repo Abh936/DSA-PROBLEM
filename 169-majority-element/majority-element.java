798371e2480a098fc54ac36e847a469ffc0e23aa
Abhishek Anand <anand141881@gmail.com>
2026-02-19 18:43:48 +0530

    Time: 8 ms (37.40%) | Memory: 55.6 MB (61.74%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);   // Sort first
+        
+        int count = 1;
+        int maxcount = 1;
+        int majority = nums[0];
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if(count > maxcount){
+                maxcount = count;
+                majority = nums[i];
+            }
+        }
+
+        return majority;   // ✅ Outside loop
+    }
+}
